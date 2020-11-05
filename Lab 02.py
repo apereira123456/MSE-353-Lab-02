031af91957b225ad64bc299d0fab759c86b477c1
apereira123456 <apereira123456@users.noreply.github.com>
2020-11-05 04:45:48 -0500

    Update Lab 02.py

--- Lab 02.py
+++ Lab 02.py
@@ -10,43 +10,55 @@ Y = np.zeros(4)
 R = 8.1345
 
 # DSC Example
-df_a = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\DSC - Example - 5K min.csv')
-df_b = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\DSC - Example - 10K min.csv')
-df_c = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\DSC - Example - 15K min.csv')
-df_d = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\DSC - Example - 20K min.csv')
+df_a = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\DSC ' \
+                    '- Example - 5K min.csv')
+df_b = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\DSC ' \
+                    '- Example - 10K min.csv')
+df_c = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\DSC ' \
+                    '- Example - 15K min.csv')
+df_d = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\DSC ' \
+                    '- Example - 20K min.csv')
 
-fig_1,ax = plt.subplots(4, sharex=True, sharey=True, figsize=(5, 6), dpi=300)
-ax[0].plot(df_a['Temperature'], df_a['Heat Flow'], color='C0', linewidth=1, label='5 K/min')
-ax[1].plot(df_b['Temperature'], df_b['Heat Flow'], color='C1', linewidth=1, label='10 K/min')
-ax[2].plot(df_c['Temperature'], df_c['Heat Flow'], color='C2', linewidth=1, label='15 K/min')
-ax[3].plot(df_d['Temperature'], df_d['Heat Flow'], color='C3', linewidth=1, label='20 K/min')
+fig_1,ax_1 = plt.subplots(4, sharex=True, sharey=True, figsize=(5, 6), dpi=300)
+ax_1[0].plot(df_a['Temperature'], df_a['Heat Flow'], color='C0', linewidth=1, label='5 K/min')
+ax_1[1].plot(df_b['Temperature'], df_b['Heat Flow'], color='C1', linewidth=1, label='10 K/min')
+ax_1[2].plot(df_c['Temperature'], df_c['Heat Flow'], color='C2', linewidth=1, label='15 K/min')
+ax_1[3].plot(df_d['Temperature'], df_d['Heat Flow'], color='C3', linewidth=1, label='20 K/min')
     
 fig_1.suptitle('DSC Scan Example Data')
-ax[3].set(xlabel=r'Temperature ($\degree$C)')
-fig_1.text(0, 0.5, 'Heat Flow (mW/mg) - Endo Down', ha='center', va='center', rotation='vertical')
+ax_1[3].set(xlabel=r'Temperature ($\degree$C)')
+ax_1[1].set(ylabel='Heat Flow (mW/mg) - Endo Down')
 for i in range(0,4):
-    ax[i].legend(loc=3)
+    ax_1[i].legend(loc=3)
 
 fig_1.savefig('DSC Example.png')
 
 
 # DSC Sample D
-df_e = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\DSC - Sample D - 10K min.csv')
-df_f = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\DSC - Sample D - 20K min.csv')
-df_g = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\DSC - Sample D - 25K min.csv')
-df_h = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\DSC - Sample D - 30K min.csv')
+df_e = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\DSC ' \
+                    '- Sample D - 10K min.csv')
+df_f = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\DSC ' \
+                    '- Sample D - 20K min.csv')
+df_g = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\DSC ' \
+                    '- Sample D - 25K min.csv')
+df_h = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\DSC ' \
+                    '- Sample D - 30K min.csv')
 
-fig_2,ax = plt.subplots(4, sharex=True, sharey=True, figsize=(5, 6), dpi=300)
-ax[0].plot(df_e['Temperature'], df_e['Heat Flow'], color='C4', linewidth=1, label='10 K/min')
-ax[1].plot(df_f['Temperature'], df_f['Heat Flow'], color='C5', linewidth=1, label='20 K/min')
-ax[2].plot(df_g['Temperature'], df_g['Heat Flow'], color='C6', linewidth=1, label='25 K/min')
-ax[3].plot(df_h['Temperature'], df_h['Heat Flow'], color='C7', linewidth=1, label='30 K/min')
+fig_2,ax_2 = plt.subplots(4, sharex=True, sharey=True, figsize=(5, 6), dpi=300)
+ax_2[0].plot(df_e['Temperature'], df_e['Heat Flow'], color='C4', linewidth=1, \
+           label='10 K/min')
+ax_2[1].plot(df_f['Temperature'], df_f['Heat Flow'], color='C5', linewidth=1, \
+           label='20 K/min')
+ax_2[2].plot(df_g['Temperature'], df_g['Heat Flow'], color='C6', linewidth=1, \
+           label='25 K/min')
+ax_2[3].plot(df_h['Temperature'], df_h['Heat Flow'], color='C7', linewidth=1, \
+           label='30 K/min')
     
 fig_2.suptitle('DSC Scan Sample D')
-ax[3].set(xlabel=r'Temperature ($\degree$C)')
-fig_2.text(0, 0.5, 'Heat Flow (mW/mg) - Endo Down', ha='center', va='center', rotation='vertical')
+ax_2[3].set(xlabel=r'Temperature ($\degree$C)')
+ax_2[1].set(ylabel='Heat Flow (mW/mg) - Endo Down')
 for i in range(0,4):
-    ax[i].legend(loc=3)
+    ax_2[i].legend(loc=3)
 
 fig_2.savefig('DSC Sample.png')
 
@@ -91,21 +103,29 @@ plt.legend()
 fig_3.savefig('Crystallization.png')
 
 # FTIR
-df_i = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\FTIR - 2 cm - 16 scans.csv')
-df_j = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\FTIR - 2 cm - 32 scans.csv')
-df_k = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\FTIR - 8 cm - 16 scans.csv')
-df_l = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\FTIR - 8 cm - 32 scans.csv')
+df_i = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\FTIR ' \
+                    '- 2 cm - 16 scans.csv')
+df_j = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\FTIR ' \
+                    '- 2 cm - 32 scans.csv')
+df_k = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\FTIR ' \
+                    '- 8 cm - 16 scans.csv')
+df_l = pd.read_csv (r'C:\Users\andre\Documents\GitHub\MSE-353-Lab-02\FTIR ' \
+                    '- 8 cm - 32 scans.csv')
 
-fig_4,ax = plt.subplots(4, sharex=True, sharey=True, figsize=(5, 6), dpi=300)
-ax[0].plot(df_i['Wavenumber'], df_i['Transmittance'], color='C0', linewidth=1, label=r'2 cm$^{-1}$ | 16 scans')
-ax[1].plot(df_j['Wavenumber'], df_j['Transmittance'], color='C1', linewidth=1, label=r'2 cm$^{-1}$ | 32 scans')
-ax[2].plot(df_k['Wavenumber'], df_k['Transmittance'], color='C2', linewidth=1, label=r'8 cm$^{-1}$ | 16 scans')
-ax[3].plot(df_l['Wavenumber'], df_l['Transmittance'], color='C3', linewidth=1, label=r'8 cm$^{-1}$ | 32 scans')
+fig_4,ax_4 = plt.subplots(4, sharex=True, sharey=True, figsize=(5, 6), dpi=300)
+ax_4[0].plot(df_i['Wavenumber'], df_i['Transmittance'], color='C0', linewidth=1, \
+           label=r'2 cm$^{-1}$ | 16 scans')
+ax_4[1].plot(df_j['Wavenumber'], df_j['Transmittance'], color='C1', linewidth=1, \
+           label=r'2 cm$^{-1}$ | 32 scans')
+ax_4[2].plot(df_k['Wavenumber'], df_k['Transmittance'], color='C2', linewidth=1, \
+           label=r'8 cm$^{-1}$ | 16 scans')
+ax_4[3].plot(df_l['Wavenumber'], df_l['Transmittance'], color='C3', linewidth=1, \
+           label=r'8 cm$^{-1}$ | 32 scans')
     
 fig_4.suptitle('FTIR')
-ax[3].set(xlabel=r'Wavenumber (cm$^{-1}$)')
-fig_4.text(0, 0.5, 'Transmittance (%)', ha='center', va='center', rotation='vertical')
+ax_4[3].set(xlabel=r'Wavenumber (cm$^{-1}$)')
+ax_4[1].set(ylabel='Transmittance (%)')
 for i in range(0,4):
-    ax[i].legend()
+    ax_4[i].legend()
 
 fig_4.savefig('FTIR.png')
